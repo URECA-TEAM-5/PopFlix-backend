2572cc944eefc2c2492871bc3804d1fa65f297a8
MINAH <tjdns7831@naver.com>
2024-11-27 16:23:33 +0900

    refactor: 중복된 이름의 보관함 에러 메시지 처리

--- src/main/java/com/popflix/common/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/popflix/common/exception/handler/GlobalExceptionHandler.java
@@ -1,13 +1,20 @@
 package com.popflix.common.exception.handler;
 
+import com.popflix.domain.storage.exception.DuplicateStorageNameException;
 import com.popflix.global.util.ApiUtil;
+import com.popflix.global.util.ApiUtil.ApiError;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestControllerAdvice
@@ -20,5 +27,13 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         ApiUtil.ApiError<String> error = ApiUtil.error(statusCode.value(), "알 수 없는 오류가 발생했습니다. 문의 바랍니다.");
         return super.handleExceptionInternal(ex, error, headers, statusCode, request);
+    }
+
+    @ExceptionHandler(DuplicateStorageNameException.class)
+    protected ResponseEntity<?> handleDuplicateStorageNameException(DuplicateStorageNameException e) {
+        log.error(e.getMessage(), e);
+        ApiError<String> error = ApiUtil.error(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+
+        return ResponseEntity.status(HttpServletResponse.SC_NOT_FOUND).body(error);
     }
 }
