06a2a25d1d2121fd318bdbe91c65256cb9ec133c
MINAH <tjdns7831@naver.com>
2024-11-27 22:20:21 +0900

    refactor: 보관함 상세 조회 영화 필드 수정

--- src/main/java/com/popflix/domain/storage/dto/GetStorageMovieResponseDto.java
+++ src/main/java/com/popflix/domain/storage/dto/GetStorageMovieResponseDto.java
@@ -1,8 +1,14 @@
 package com.popflix.domain.storage.dto;
 
+import com.popflix.domain.movie.entity.Director;
 import com.popflix.domain.movie.entity.Movie;
+import com.popflix.domain.personality.entity.Genre;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
@@ -11,14 +17,24 @@ public class GetStorageMovieResponseDto {
     private Long id;
     private String title;
     private String posterPath;
-    private String overview;
-    private Long likeCount;
+    private List<String> genres;
+    private String director;
+    private LocalDate releaseDate;
 
     public GetStorageMovieResponseDto(Movie movie) {
         this.id = movie.getId();
         this.title = movie.getTitle();
         this.posterPath = movie.getPosterPath();
-        this.overview = movie.getOverview();
-        this.likeCount = movie.getLikeCount();
+        this.releaseDate = movie.getReleaseDate();
+
+        // 장르 정보를 가져오기
+        this.genres = movie.getMovieGenres().stream()
+                .map(movieGenre -> movieGenre.getGenre().getName())
+                .collect(Collectors.toList());
+
+        // 감독 정보를 가져오기
+        this.director = movie.getMovieDirectors().stream()
+                .map(movieDirector -> movieDirector.getDirector().getName())
+                .collect(Collectors.joining(", "));
     }
 }
