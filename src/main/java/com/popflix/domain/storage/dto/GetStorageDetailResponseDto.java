3c025e1fbf8f50ad0c00c1a3308aa8959fca90af
MINAH <tjdns7831@naver.com>
2024-11-27 21:41:48 +0900

    chore: dto 파일명 변경

--- src/main/java/com/popflix/domain/storage/dto/GetStorageDetailResponseDto.java
+++ src/main/java/com/popflix/domain/storage/dto/GetStorageDetailResponseDto.java
@@ -1,24 +1,40 @@
 package com.popflix.domain.storage.dto;
 
 import com.popflix.domain.movie.entity.Movie;
+import com.popflix.domain.storage.entity.Storage;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
 public class GetStorageDetailResponseDto {
 
-    private Long id;
-    private String title;
-    private String posterPath;
-    private String overview;
-    private Long likeCount;
+    private GetStorageResponseDto storage;
+    private List<GetStorageMovieResponseDto> movies;
 
-    public GetStorageDetailResponseDto(Movie movie) {
-        this.id = movie.getId();
-        this.title = movie.getTitle();
-        this.posterPath = movie.getPosterPath();
-        this.overview = movie.getOverview();
-        this.likeCount = movie.getLikeCount();
+    public GetStorageDetailResponseDto(Storage storage, List<Movie> movies) {
+        this.storage = new GetStorageResponseDto(storage);
+        this.movies = movies.stream()
+                .map(GetStorageMovieResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
+
+/*
+1. 보관함 명
+2. 만든이
+3. 만든 날짜
+4. 보관함 좋아요 수
+5. 보관함 좋아요 조회
+6. 보관함 소개글
+7. 보관함 이미지
+8. 영화 리스트
+    8-1. 영화 포스터
+    8-2. 영화 제목
+    8-3. 장르
+    8-4. 감독(?)
+    8-5. 개봉일
+ */
---
+++ src/main/java/com/popflix/domain/storage/dto/GetStorageMovieResponseDto.java
@@ -0,0 +1,24 @@
+package com.popflix.domain.storage.dto;
+
+import com.popflix.domain.movie.entity.Movie;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GetStorageMovieResponseDto {
+
+    private Long id;
+    private String title;
+    private String posterPath;
+    private String overview;
+    private Long likeCount;
+
+    public GetStorageMovieResponseDto(Movie movie) {
+        this.id = movie.getId();
+        this.title = movie.getTitle();
+        this.posterPath = movie.getPosterPath();
+        this.overview = movie.getOverview();
+        this.likeCount = movie.getLikeCount();
+    }
+}
