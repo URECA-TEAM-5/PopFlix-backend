bcba50c660a2ec722298bf38f4634e1acee4b66a
MINAH <tjdns7831@naver.com>
2024-11-28 13:19:45 +0900

    refactor: 보관함 영화 추가 시 중복 등록 예외 처리

--- src/main/java/com/popflix/domain/storage/repository/MovieStorageRepository.java
+++ src/main/java/com/popflix/domain/storage/repository/MovieStorageRepository.java
@@ -2,6 +2,7 @@ package com.popflix.domain.storage.repository;
 
 import com.popflix.domain.movie.entity.Movie;
 import com.popflix.domain.storage.entity.MovieStorage;
+import com.popflix.domain.storage.entity.Storage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +17,6 @@ public interface MovieStorageRepository extends JpaRepository<MovieStorage, Long
              WHERE ms.storage.id = :storageId
             """)
     List<Movie> findMoviesByStorageId(@Param("storageId") Long storageId);
+
+    boolean existsByStorageAndMovie(Storage storage, Movie movie);
 }
