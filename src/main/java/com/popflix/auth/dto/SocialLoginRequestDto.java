8f1cb217c864000088d24e29a06b471c77cbc3b2
JUNHA CHOI <choijh0309@naver.com>
2024-11-30 17:56:54 +0900

    remove : 회원가입 구현 삭제

--- src/main/java/com/popflix/auth/dto/SocialLoginRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.popflix.auth.dto;
-
-import lombok.Data;
-
-@Data
-public class SocialLoginRequestDto {
-    private String socialToken; // 소셜 인증 토큰
-    private String provider;    // 제공자 (Google, Naver)
-}
