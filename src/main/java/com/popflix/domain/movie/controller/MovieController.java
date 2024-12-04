93fbd2fc52c62cb1920254115205cc62d47ad93b
MINAH <tjdns7831@naver.com>
2024-12-04 10:39:54 +0900

    test: CI/CD 테스트-2

--- src/main/java/com/popflix/domain/movie/controller/MovieController.java
+++ src/main/java/com/popflix/domain/movie/controller/MovieController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-// cicd 테스트 - 1
+// cicd 테스트 - 2
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/movies")
