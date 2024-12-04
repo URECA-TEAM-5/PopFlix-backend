34b827150973c1b26b2205bf87d094f8cf66ba64
MINAH <tjdns7831@naver.com>
2024-12-04 23:22:39 +0900

    test: cicd 테스트 - 서비스 실행이 안되는 문제 1

--- src/main/java/com/popflix/domain/movie/controller/MovieController.java
+++ src/main/java/com/popflix/domain/movie/controller/MovieController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-// cicd 테스트 - 5
+// cicd 테스트 - 6
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/movies")
