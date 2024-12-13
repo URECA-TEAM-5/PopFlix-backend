0fe7a3038d73fd14ae3c5c6370f1698ee47e5af3
MINAH <tjdns7831@naver.com>
2024-12-13 17:23:50 +0900

    refactor: 보관함 개봉 안 된 영화 제외 코드 추가

--- src/main/java/com/popflix/domain/movie/repository/MovieRepository.java
+++ src/main/java/com/popflix/domain/movie/repository/MovieRepository.java
@@ -11,30 +11,39 @@ import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository<Movie, Long> {
     @Query("""
-                SELECT m FROM Movie m
-                LEFT JOIN m.movieCasts mc
-                LEFT JOIN mc.cast c
-                LEFT JOIN m.movieDirectors md
-                LEFT JOIN md.director d
-                WHERE m.title LIKE CONCAT('%', :keyword, '%')
-                OR c.name LIKE CONCAT('%', :keyword, '%')
-                OR d.name LIKE CONCAT('%', :keyword, '%')
-                ORDER BY CASE
-                    WHEN m.title LIKE CONCAT('%', :keyword, '%') THEN 0
-                    WHEN c.name LIKE CONCAT('%', :keyword, '%') THEN 1
-                    WHEN d.name LIKE CONCAT('%', :keyword, '%') THEN 2
-                    ELSE 3 END, m.id ASC
-            """)
+        SELECT m FROM Movie m
+        LEFT JOIN m.movieCasts mc
+        LEFT JOIN mc.cast c
+        LEFT JOIN m.movieDirectors md
+        LEFT JOIN md.director d
+        WHERE (m.title LIKE CONCAT('%', :keyword, '%')
+        OR c.name LIKE CONCAT('%', :keyword, '%')
+        OR d.name LIKE CONCAT('%', :keyword, '%'))
+        AND m.releaseDate <= CURRENT_DATE
+        ORDER BY CASE
+            WHEN m.title LIKE CONCAT('%', :keyword, '%') THEN 0
+            WHEN c.name LIKE CONCAT('%', :keyword, '%') THEN 1
+            WHEN d.name LIKE CONCAT('%', :keyword, '%') THEN 2
+            ELSE 3 END, m.id ASC
+    """)
     Page<Movie> findByKeyword(@Param("keyword") String keyword, Pageable pageable);
 
     @Query("""
-            SELECT m
-              FROM Movie m
-              JOIN m.movieGenres mg
-              JOIN mg.genre g
-             WHERE g.name = :genre
-            """)
+        SELECT m
+          FROM Movie m
+          JOIN m.movieGenres mg
+          JOIN mg.genre g
+         WHERE g.name = :genre
+           AND m.releaseDate <= CURRENT_DATE
+    """)
     Page<Movie> findByGenre(@Param("genre") String genre, Pageable pageable);
+
+    @Query("""
+        SELECT m
+          FROM Movie m
+         WHERE m.releaseDate <= CURRENT_DATE
+    """)
+    Page<Movie> findAllWithReleaseDateBeforeNow(Pageable pageable);
 
     Optional<Movie> findByTitle(String title);
 }
